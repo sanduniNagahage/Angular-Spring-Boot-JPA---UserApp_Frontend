a9f1c7c7791db57a20d20e80422bdf7b85184de6
Sanduni Nagahage <sanduninagahage@gmail.com>
2020-05-07 22:31:27 +0530

    created  the user services and the model class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'userAppFrontend';
+  title: string;
+
+  constructor() {
+    this.title = 'Spring Boot - Angular Application';
+  }
 }
---
+++ src/app/user-service.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  private userUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.userUrl = 'http://localhost:8080/users';
+  }
+
+  public findAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.userUrl);
+  }
+
+  public save(user: User) {
+    return this.http.post<User>(this.userUrl, user);
+  }
+}
